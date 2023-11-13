a7d9a41b78ecbcc4bebd24805126944ef6806a07
Marc Mullally <marcmullally0@gmail.com>
2023-11-13 11:07:55 -0500

    Initial Commit

---
+++ Meme Maker Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme Maker Project
+//
+//  Created by 2b on 11/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
